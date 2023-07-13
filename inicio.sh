675a5a5061c45c19e8081512fd18a194a988182e
gonzaloctorres <77621940+gonzaloctorres@users.noreply.github.com>
2023-07-13 00:13:32 -0300

    Update inicio.sh

--- inicio.sh
+++ inicio.sh
@@ -12,3 +12,7 @@ wget https://raw.githubusercontent.com/gonzaloctorres/backup/main/backup.sh -O /
 chmod 777 /script/backup.sh
 echo "0 1 * * * wget https://raw.githubusercontent.com/gonzaloctorres/backup/main/backup.sh -O /script/backup.sh >/dev/null 2>&1" >> /var/spool/cron/root
 echo "0 0,6,12,18 * * * /script/backup.sh >/dev/null 2>&1" >> /var/spool/cron/root
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+./aws/install
+aws configure
