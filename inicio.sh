ab18db5ec80e56d244cf5174aa37aaa09650c8f2
gonzaloctorres <77621940+gonzaloctorres@users.noreply.github.com>
2023-07-13 00:11:59 -0300

    Update inicio.sh

--- inicio.sh
+++ inicio.sh
@@ -1,8 +1,14 @@
 # !/bin/bash
 ######################################################
-# Script de backup 
-# versao: v1.7
-# Ultima atualização: 13/07/2023
+# Script que cria a estrutura necessaria
 ######################################################
 
-. ./parametros.conf
+mkdir /backup
+mkdir /script/log -p
+touch /backup/id
+echo "0" > /backup/id
+wget https://raw.githubusercontent.com/gonzaloctorres/backup/main/parametros.conf -O /script/parametros.conf
+wget https://raw.githubusercontent.com/gonzaloctorres/backup/main/backup.sh -O /script/backup.sh
+chmod 777 /script/backup.sh
+echo "0 1 * * * wget https://raw.githubusercontent.com/gonzaloctorres/backup/main/backup.sh -O /script/backup.sh >/dev/null 2>&1" >> /var/spool/cron/root
+echo "0 0,6,12,18 * * * /script/backup.sh >/dev/null 2>&1" >> /var/spool/cron/root
